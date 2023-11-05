d90c9810a12373e28dbff69bfe24f9d03fc0844e
Thomas Buchinger <thomas.buchinger@outlook.com>
2023-11-05 16:36:43 +0100

    Remove Unittests

--- ui/tests/utils.test.ts
+++ ui/tests/utils.test.ts
@@ -1,13 +1,7 @@
 // import { is_ip_internal } from "../app/lib/utils";
 
-// describe('is_ip_internal() function', () => {
-//   test('should be true for public ip', () => {
-//     expect(is_ip_internal("1.2.3.4")).toBe(false);
-//   });
-//   test('should be true for reverse-proxy-ip', () => {
-//     expect(is_ip_internal("10.0.0.21")).toBe(false);
-//   });
-//   test('should be false for any other private ip', () => {
-//     expect(is_ip_internal("10.0.0.1")).toBe(true);
-//   });
-// });
+describe('dummy test', () => {
+  test('is always true', () => {
+    expect(true).toBe(true);
+  });
+});
