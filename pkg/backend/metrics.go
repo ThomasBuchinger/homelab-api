c2c236727c6a7395710e32cc9243895c2b24cb50
Thomas Buchinger <thomas.buchinger@outlook.com>
2023-11-25 01:00:41 +0100

    Add geoip metrics

--- pkg/backend/metrics.go
+++ pkg/backend/metrics.go
@@ -8,11 +8,11 @@ import (
 	"github.com/thomasbuchinger/homelab-api/pkg/common"
 )
 
-var MetricGeoipRequestsTotal *prometheus.CounterVec = promauto.NewCounterVec(prometheus.CounterOpts{
+var metricGeoipRequestsTotal *prometheus.CounterVec = promauto.NewCounterVec(prometheus.CounterOpts{
 	Name: "homelabapi_geoip_requests_total",
 	Help: "Total Requests seen by the ExtAuthz handler by country",
 }, []string{"Latitude", "Longitude", "Country", "City"})
 
 func LogGeoipRequest(loc common.GeoipLookup) {
-	MetricGeoipRequestsTotal.WithLabelValues(fmt.Sprint(loc.Latitude), fmt.Sprint(loc.Longitude), loc.Country, loc.CityName)
+	metricGeoipRequestsTotal.WithLabelValues(fmt.Sprint(loc.Latitude), fmt.Sprint(loc.Longitude), loc.Country, loc.CityName).Inc()
 }
